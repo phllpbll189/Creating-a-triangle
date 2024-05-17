61f9abad989e94cac3e010e9cd5982e72af91bb8
phllpbll189 <phillip@devbell.net>
2024-05-17 02:02:40 -0500

    learned how to render multiple triangles while only using unique vertices

--- src/main.cpp
+++ src/main.cpp
@@ -24,15 +24,18 @@ const char* fragmentShaderSource = "#version 330 core\n"
 #define HORIZONTAL 0
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
 };
 
 void updateTriangle(int axis, float direction) {
-	cout << axis << " " << direction << endl;
 	for (int i = axis; i < 9; i = i + 3) {
-		cout << i << endl;
 		vertices[i] = vertices[i] + direction;
 	}
 }
@@ -98,55 +101,60 @@ int main() {
 	glAttachShader(shaderProgram, fragShader);
 	glLinkProgram(shaderProgram);
 
-
-	//===========TEST TO MAKE SURE IT WORKS
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragShader);
+	
 	int success;
 	char infoLog[512];
 
+	//MAKE SURE IT WORKS
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		cout << "ERROR::PROGRAM::SHADER::LINKING_FAILED\n" << infoLog << endl;
-	}
-
-
+	} 
 	glUseProgram(shaderProgram);
-	//SHADERS NO LONGER NEEDED AFTER LINK
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragShader);
 
-	//===========MAKING BUFFERS
-	unsigned int VAO, VBO;
-	glGenVertexArrays(1, &VAO);
+	//BUFFERS
+	unsigned int VAO, VBO, EBO;
 	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &VAO);
 
-	//generate buffer from memory(VBO) and then bind to buffer and add triangle.
+	glBindVertexArray(VAO);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	//set vertex attributes pointers (since vertexes are an array of numbers we tell the computer how to use them to draw)
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//use our shader program to render
-
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	//render loop
 	while (!glfwWindowShouldClose(window)) {
-		update(vertices, window);
+		//update(vertices, window);
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 
 
 
