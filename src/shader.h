7b39479e19cfcd719feb80acc287cfe0e1cd9941
phllpbll189 <phillip@devbell.net>
2024-05-19 20:26:43 -0500

    adds a class and both shaders in their own file

---
+++ src/shader.h
@@ -0,0 +1,115 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include<glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class Shader {
+    public:
+        unsigned int ID;
+
+        Shader(const char* vertexPath, const char* fragmentPath)
+        {
+            string vertexCode;
+            string fragmentCode;
+            ifstream vShaderFile;
+            ifstream fShaderFile;
+
+            vShaderFile.exceptions (ifstream::failbit | ifstream::badbit);
+            fShaderFile.exceptions (ifstream::failbit | ifstream::badbit);
+            try 
+            {
+                vShaderFile.open(vertexPath);
+                fShaderFile.open(fragmentPath);
+                stringstream vShaderStream, fShaderStream;
+
+                vShaderStream << vShaderFile.rdbuf();
+                fShaderStream << fShaderFile.rdbuf();
+
+                vShaderFile.close();
+                fShaderFile.close();
+
+                vertexCode = vShaderStream.str();
+                fragmentCode = fShaderStream.str();
+            } 
+            catch (std::ifstream::failure &e)
+            {
+                cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << e.what() << endl;
+            }
+
+            const char* vShaderCode = vertexCode.c_str();
+            const char* fShaderCode = vertexCode.c_str();
+
+            unsigned int vertex, fragment;
+
+            vertex =   glCreateShader(GL_VERTEX_SHADER);
+            fragment = glCreateShader(GL_FRAGMENT_SHADER); 
+
+            glShaderSource(vertex, 1, &vShaderCode, NULL);
+            glShaderSource(fragment, 1, &fShaderCode, NULL);
+            
+            glCompileShader(fragment);
+            glCompileShader(vertex);
+
+            checkCompileErrors(fragment, "FRAGMENT");
+            checkCompileErrors(vertex, "VERTEX");
+            
+            ID = glCreateProgram();
+            glAttachShader(ID, vertex);
+            glAttachShader(ID, fragment);
+            glLinkProgram(ID);
+
+            checkCompileErrors(ID, "PROGRAM");
+
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
+        };
+        //use a shader
+        void use()
+        {
+            glUseProgram(ID);
+        };
+
+        //set uniforms
+        void setBool(const std::string &name, bool value) const
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
+        };
+
+        void setInt(const std::string &name, int value) const 
+        {
+            glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+        };
+
+        void setFloat(const std::string &name, float value) const
+        {
+            glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+        };
+
+        private:
+            void checkCompileErrors(unsigned int shader, string type)
+            {
+                int success;
+                char infoLog[1024];
+                if (type != "PROGRAM")
+                {
+                    glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                    cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "/n-------------------------------------------------" << endl;
+                }
+                else 
+                {
+                    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+                    if(!success){
+                        glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                        cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "/n" << infoLog << "/n------------------------------------------------" << endl; 
+                    }
+                };
+            }
+};
+#endif
